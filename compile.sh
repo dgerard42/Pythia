278f5259f8a9da8d77a4639b8302d5929c760927
Daniella Gerard <daniella4422@gmail.com>
2017-10-25 18:48:01 -0700

    still learning how to use shit

--- compile.sh
+++ compile.sh
@@ -1,3 +1,15 @@
-gcc -o test test.c \
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    compile.sh                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dgerard <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/10/24 16:40:33 by dgerard           #+#    #+#              #
+#    Updated: 2017/10/25 18:15:14 by dgerard          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+gcc -o listen listen.c \
     -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" \
     `pkg-config --cflags --libs pocketsphinx sphinxbase`
---
+++ listen.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   listen.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgerard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/24 16:39:03 by dgerard           #+#    #+#             */
+/*   Updated: 2017/10/24 16:39:08 by dgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "listen.h"
+
+int				main(void)
+{
+	system("pocketsphinx_continuous -inmic yes -logfn /dev/null");
+	// -inmic yes : listen through mic input
+	// -logfn /dev/null : silence log output onto terminal
+}
