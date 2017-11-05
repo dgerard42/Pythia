9d1f7d06e479efa7998ee5848f70f546e647da26
Daniella Gerard <daniella4422@gmail.com>
2017-11-04 19:44:22 -0700

    uhg

--- listen.c
+++ listen.c
@@ -10,25 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "listen.h"
+# include <stdlib.h>
 
 int				main(void)
 {
-//	bool	run;
-//	char	*line;
-
-//	run = true;
-//	line = NULL;
-//	while (run == true)
-	{
-		system("pocketsphinx_continuous -inmic yes -logfn /dev/null\
-			-lm support/9005.lm -dict support/9005.dic");
-			// -inmic yes : listen through mic input
-			// -logfn /dev/null : silence log output onto terminal
-//		get_next_line(1, &line);
-//		printf("program output: %s\n", line);
-//		if (line == "poop")
-//			break;
-	}
-//	ft_memdel((void**)&line);
+	system("pocketsphinx_continuous -inmic yes -logfn /dev/null\
+		-lm support/lang_mod_0.lm -dict support/dic_0.dic");
+	// -logfn silence log
+	// -inmic listen through mic
+	// -lm specify custom language model
+	// -dict specify custom dictionary
 }
